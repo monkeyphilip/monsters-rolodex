19f474eb777ffed2f94c2f5ef1f90070f789e061
monkeyphilip <monkey.philip32@gmail.com>
2020-09-12 21:50:25 -0400

    Updates

---
+++ precache-manifest.9641bceea5cd84117105248153684a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f94a5bf92df5f418ab377e10125fe4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f002c3aa8d1e4ebf65a6",
+    "url": "/monsters-rolodex/static/css/main.66dcecf1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f002c3aa8d1e4ebf65a6",
+    "url": "/monsters-rolodex/static/js/main.145164b2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
